77a82320bcef0ed77cf2666be19cea6cd5eaa1be
zoeyj1 <87721216+zoeyj1@users.noreply.github.com>
2026-04-01 20:23:25 -0400

    Add files via upload

---
+++ zoeyj-increment4/static/script.js
@@ -0,0 +1,92 @@
+// var x = 5;
+// var y = 7;
+// var z = x + y;
+// console.log(z);
+// var A = "Hello ";
+// var B = "World!";
+// var C = A + B;
+// console.log(C);
+
+// function sumnPrint(x1, x2) {
+//     var sum = x1 + x2;
+//     console.log(sum);
+// }
+
+// if (C.length > z) {
+//     console.log(C);
+// } if (C.length < z) {
+//     console.log(z);
+// } else {
+//     console.log("good job!");
+// }
+
+// L1 = ["Watermelon","Pineapple","Pear","Banana"];
+// L2 = ["Apple","Banana","Kiwi","Orange"];
+
+// function findTheBanana(fruitArray) {
+//     for (var i = 0; i < fruitArray.length; i++) {
+//         if (fruitArray[i] === "Banana") {
+//             alert("Found Banana!");
+//         }
+//     }
+// }
+
+var now = new Date();
+var hour = now.getHours();
+function greeting(x) {
+    var greetingElement = document.getElementById("greeting");
+    if (greetingElement == null) {
+        return;
+    }
+    if (x < 5 || x >= 20) {
+        greetingElement.innerHTML = "Good Night!";
+    } else if (x < 12) {
+        greetingElement.innerHTML = "Good Morning!";
+    } else if (x < 18) {
+        greetingElement.innerHTML = "Good Afternoon!";
+    } else {
+        greetingElement.innerHTML = "Good Evening!";
+    }
+}
+greeting(hour);
+
+function addYear() {
+    var year = new Date().getFullYear();
+    document.getElementById("copyYear").innerHTML = year;
+}
+
+function ActiveNav() {
+    // Retrieve all navigation links
+    const navLinks = document.querySelectorAll('nav a');
+
+    // Iterate over each link
+    navLinks.forEach(link => {
+        if (window.location.href === link.href) {
+            // Add the 'active' class to highlight the current page
+            link.classList.add("active");
+        }
+    });
+}
+
+// Execute the function to set the active navigation link on page load
+ActiveNav();
+
+$("#readLess").click(function() {
+    $("#longIntro").hide();
+    $("#readLess").hide();
+    $("#readMore").show();
+});
+
+$("#readMore").click(function(){
+    $("#longIntro").show();
+    $("#readLess").show();
+    $("#readMore").hide();
+});
+
+$(".buyNow").click(function(){
+    $("#checkoutForm").show();
+});
+
+$("#submitButton").click(function(){
+    $("#submitMessage").show();
+});
